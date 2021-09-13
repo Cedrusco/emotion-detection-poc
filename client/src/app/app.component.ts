afa3a6783cc05cd839f039eb6a1eecf5ba02089b
Isaias Pomales <impomales@gmail.com>
2021-09-13 14:58:02 -0400

    stream webcam in video element

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Emotion Detection POC';
+  videoNativeElement;
+  @ViewChild('userVideoStream') userVideoStream;
+
+  ngOnInit() {
+    this.videoNativeElement = <HTMLVideoElement>this.userVideoStream.nativeElement;
+    navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+      this.videoNativeElement.srcObject = stream;
+    })
+  }
 }
