33e282d04bdde8ba4a8ea078230e055c09e0298f
Isaias Pomales <impomales@gmail.com>
2021-09-17 16:16:36 -0400

    define route for backend services

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use('/api', require('./src/api'));
+
 app.get('/', (req, res) => {
   res.send('Emotion Detection POC');
 });
