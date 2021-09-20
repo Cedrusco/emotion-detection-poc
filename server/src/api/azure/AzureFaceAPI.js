28e3dd44a2ec5a37b00be1c952e696a2489e8dec
Isaias Pomales <impomales@gmail.com>
2021-09-20 14:45:44 -0400

    define analyze face function that returns emotion results

--- server/src/api/azure/AzureFaceAPI.js
+++ server/src/api/azure/AzureFaceAPI.js
@@ -2,15 +2,38 @@ const { FaceClient } = require('@azure/cognitiveservices-face');
 const { ApiKeyCredentials } = require('@azure/ms-rest-js');
 
 class AzureFaceAPI {
-  client;
-
   constructor() {
-    const credentials = new ApiKeyCredentials({
-      inHeader: {
-        'Ocp-Apim-Subscription-Key': process.env.AZURE_FACE_APIKEY,
-      },
-    });
-    this.client = new FaceClient(credentials, process.env.AZURE_FACE_URL);
+    try {
+      const credentials = new ApiKeyCredentials({
+        inHeader: {
+          'Ocp-Apim-Subscription-Key': process.env.AZURE_FACE_APIKEY,
+        },
+      });
+      this.client = new FaceClient(credentials, process.env.AZURE_FACE_URL);
+    } catch (e) {
+      console.error(e);
+      throw new Error('AzureFaceAPi failed to connect');
+    }
+  }
+
+  async analyzeFace(url) {
+    let result;
+    try {
+      result = await this.client.face.detectWithUrl(url, {
+        detectionModel: 'detection_01',
+        faceIdTimeToLive: 86400,
+        recognitionModel: 'recognition_04',
+        returnFaceAttributes: ['emotion'],
+        returnfaceId: true,
+        returnFaceLandmarks: false,
+        returnRecognitionModel: false,
+      });
+    } catch (e) {
+      console.error(e);
+      throw new Error('analyzeFace method failed')
+    }
+
+    return result;
   }
 }
 
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,14 +2,21 @@ const AzureFaceAPI = require('../api/azure/AzureFaceAPI');
 
 const router = require('express').Router();
 
-router.post('/analyze', (req, res) => {
+router.post('/analyze', async (req, res) => {
+  try {
   const { voiceAudio, faceImages } = req.body;
 
   console.log(voiceAudio, faceImages);
 
   const faceClient = new AzureFaceAPI();
+  const imageUrl = faceImages[0];
 
-  res.json({ voiceAudio, faceImages, faceClient });
+  const result = await faceClient.analyzeFace(imageUrl);
+
+  res.json({ voiceAudio, faceImages, result });
+  } catch (e) {
+    res.json(e);
+  }
 });
 
 module.exports = router;
